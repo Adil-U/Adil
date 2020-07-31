a43147f8c40c451a340ef5b8c2ebde6391d2c761
Adil-U <adilutepbergenov727@gmail.com>
2020-07-31 17:40:43 +0600

    add functions,loops and square

---
+++ functions.py
@@ -0,0 +1,10 @@
+#functions
+#define - opredelit
+def square (x):
+	return x * x
+
+#for i in range (10):
+	#print(f"The square of {i} is {square(i)}")
+
+z = int(input("Insert the number: "))
+print (f"The square of {z} is {square(z)}")
---
+++ loops.py
@@ -0,0 +1,11 @@
+# range-diapozon chisel
+# vyvod kazhdogo i(chisla) v diapozone do 99
+for i in range(2):
+	print (i)
+#slovar - dictionaries.py
+houses = {"Harry":"Gryffindor", "Draco":"Slytherin"}
+
+houses["Hermione"] = "Gryffindor"
+
+print (houses)
+
