f45431075b0c306e4b8ce5a8a21078ba927de227
Adil-U <adilutepbergenov727@gmail.com>
2020-07-31 18:42:10 +0600

    Add classes and izmeneniya v square

--- square.py
+++ square.py
@@ -2,3 +2,24 @@ from functions import square
 
 for i in range (10):
 	print(f"The square of {i} is {square(i)}")
+
+#the second way
+#import functions  
+#for i in range (10):
+#	print(f"The square of {i} is {functions.square(i)}")
+
+
+
+
+#HOW TO SORT A DICT
+people = [
+	{"name":"Adil","Hobby":"Football"},
+	{"name": "Dias", "Hobby":"CS:GO"},
+	{"name": "Draco", "Hobby":"Run"}
+
+]
+def so(person):
+	return person["Hobby"]
+people.sort(key = so)
+
+print(people)
